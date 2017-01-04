6cfe0027587bbca958959c37465a2d22eb474e15
Nico Hoogervorst <n.g.c.hoogervorst@gmail.com>
2017-01-04 14:58:37 +0100

    Upgrade Eternal

--- lib/phoenix_example.ex
+++ lib/phoenix_example.ex
@@ -6,7 +6,7 @@ defmodule PhoenixExample do
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
-    Eternal.new(SimpleMemCache, [ :named_table, :set, { :read_concurrency, true }, { :write_concurrency, true }])
+    Eternal.start_link(SimpleMemCache, [ :set, { :read_concurrency, true }, { :write_concurrency, true }])
 
     children = [
       # Start the endpoint when the application starts
